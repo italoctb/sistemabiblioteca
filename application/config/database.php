163cde549c1784ccf08d7223e47be2dbafbb2122
italoctb <italobarroscontato@gmail.com>
2018-11-17 17:18:32 -0300

    editando credenciais do banco de dados-3

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => 'italobarros2',
+	'hostname' => '',
+	'username' => '',
+	'password' => '',
 	'database' => 'equipe385116',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
